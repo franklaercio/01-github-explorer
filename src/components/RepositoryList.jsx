5095ce3b1e2850db83c1a4b4f69c353c41dc7003
Frank Laercio <franklaercio@gmail.com>
2021-03-11 01:06:59 -0300

    Adding request http

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,29 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss'
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/franklaercio'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]); 
+
+    /**
+     * Quando o parâmetro está vazio ele é executado uma única vez
+     * Quando é colocado a dependência ele é executado a cada mudança dessa variável
+     */
+    useEffect(() => {
+        fetch('https://api.github.com/users/franklaercio/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem />
+                {repositories.map(repo => {
+                    return <RepositoryItem key={repo.name} repository={repo} />
+                })}
             </ul>
         </section>
     );
